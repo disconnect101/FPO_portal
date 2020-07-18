77fe42101d44441912738abc6759a8cb3f7efc90
Aditya Gupta <adityagupta4114@gmail.com>
2020-07-18 21:06:22 +0530

    static, templates, urls, views

--- RuralCaravan/fpo/views.py
+++ RuralCaravan/fpo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def test(request):
+    context = {
+        test: 'test',
+    }
+    return render(request, 'fpo/test.html', context)
