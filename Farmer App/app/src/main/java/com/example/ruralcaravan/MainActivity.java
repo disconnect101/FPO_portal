76c4d9753eaa3b5f2b70e08c0a1b31b72af2342b
Manas Uniyal <manasuniyal100@gmail.com>
2020-07-19 02:42:37 +0530

    Added toolbar and dummy navigation drawer

--- Farmer App/app/src/main/java/com/example/ruralcaravan/MainActivity.java
+++ Farmer App/app/src/main/java/com/example/ruralcaravan/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.ruralcaravan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        drawerLayout = findViewById(R.id.drawer_layout);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home: {
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            }
+            case R.id.profile: {
+                Toast.makeText(MainActivity.this, "Profile clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            case R.id.notification: {
+                Toast.makeText(MainActivity.this, "Notification clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
