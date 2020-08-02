ae807345e020ec340070f37025875942d21fec2c
Aditya Gupta <adityagupta4114@gmail.com>
2020-08-02 14:42:30 +0530

    rsvp meeting, meeting token

--- RuralCaravan/farmer/api/MeetingsNewsSchemes/views.py
+++ RuralCaravan/farmer/api/MeetingsNewsSchemes/views.py
@@ -1,6 +1,6 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from farmer.models import Meetings, MeetingToken
+from farmer.models import Meetings, MeetingToken, Farmer
 from rest_framework.response import Response
 from datetime import datetime
 from farmer.api.utils import statuscode
@@ -10,13 +10,17 @@ import json
 @api_view(['GET', ])
 @permission_classes((IsAuthenticated, ))
 def meetings(request):
+    user = request.user
+    farmer = Farmer.objects.get(user=user)
     currDate = datetime.now().date()
-    meetings = Meetings.objects.filter(date__gte=currDate)
+    meetings = Meetings.objects.filter(date__gte=currDate).values()
 
     meetingsList = []
     for meeting in meetings:
-        serialized = MeetingsSerializer(meeting)
-        meetingsList.append(serialized.data)
+        #serialized = MeetingsSerializer(meeting)
+        meetingtoken = MeetingToken.objects.get(farmer=farmer, meeting_id=meeting.get('id'))
+        meeting.update( { 'meetingtoken': meetingtoken.token_number } )
+        meetingsList.append(meeting)
 
     data = { 'data': meetingsList }
     return Response(statuscode('0', data))
@@ -31,6 +35,8 @@ def rsvpMeeting(request):
     token = MeetingToken.objects.get(token_number=meetingToken)
     token.has_rsvped = True
     token.save()
+
+    return Response(statuscode('0'))
 
 
 
